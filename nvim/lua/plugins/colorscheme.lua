59deea7b29815a5d23740be74bc4a679b631af13
draganstefanovic12 <stefanovicdragan112@gmail.com>
2024-11-13 22:34:41 +0100

    dotfiles

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"catppuccin/nvim",
+		lazy = true,
+    name="catppuccin",
+		priority = 1000,
+		opts = function()
+			return {
+				transparent_background = true,
+			}
+		end,
+	},
+}
